ce8aabc16411c83bfb84a45b0cdf7757a4b9e84e
katzkatrice <katzkatrice@gmail.com>
2026-08-17 10:19:51 +0700

    Add portable PDO database class

---
+++ src/Database.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+final class Database
+{
+    private PDO $pdo;
+
+    public function __construct(array $config)
+    {
+        $dsn = sprintf(
+            'mysql:host=%s;port=%d;dbname=%s;charset=utf8mb4',
+            $config['host'],
+            $config['port'],
+            $config['database']
+        );
+
+        $this->pdo = new PDO($dsn, $config['username'], $config['password'], [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+    }
+
+    public function connection(): PDO
+    {
+        return $this->pdo;
+    }
+}
